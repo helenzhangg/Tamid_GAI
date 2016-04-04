c6a2a4541653fab49de49e0b30b1d5a84fb66d84
helenzhangg <h.zhang2021@gmail.com>
2016-04-04 19:01:28 -0400

    first draft

---
+++ Insider/CreateAccount.swift
@@ -0,0 +1,12 @@
+//
+//  CreateAccount.swift
+//  Insider
+//
+//  Created by Diana Chen on 11/23/15.
+//  Copyright © 2015 Joe Brandy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
---
+++ Insider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Insider
+//
+//  Created by Joe Brandy on 11/2/15.
+//  Copyright (c) 2015 Joe Brandy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
